a083d1887aa4d057c3fdeb2233f6f377b09fb43d
hoseyn7832 <hasanzade.hoseyn27@gmail.com>
2025-06-05 13:24:21 +0330

    aaa

--- obj/Debug/net8.0/backtimetracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/backtimetracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backtimetracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b196960e4557cfcdbb64f87d1e395d88b9f8893")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47db770cf88bd9e89529e2cdc4e15e9ddf9ac59d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backtimetracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backtimetracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
